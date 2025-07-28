131e569c636dca383f0e89d885d5ded62da44bcb
klausmic <vedhanthmugalihal2082@gmail.com>
2025-07-28 12:26:54 +0530

    third

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -10,7 +10,7 @@ export default function Hero() {
         transition={{ duration: 1 }}
         className="display-4 fw-bold"
       >
-        Hey, I'm Adithya 👋
+        Hey, I'm Vedanth 👋
       </motion.h1>
       <motion.p
         initial={{ opacity: 0, y: 20 }}
